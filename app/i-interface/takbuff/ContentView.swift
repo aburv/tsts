b0f64f4f8f2029dfbc7afd5fec460c5086cc756c
guru-aburv <gurunathan.r@aburv.com>
2023-08-23 18:43:13 +0530

    [Project setup] Android interface and Ios interface

---
+++ app/i-interface/takbuff/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  takbuff
+//
+//  Created by Gurunathan on 23/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        return Text("hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/i-interface/takbuff/takbuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  takbuffApp.swift
+//  takbuff
+//
+//  Created by Gurunathan on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct takbuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
