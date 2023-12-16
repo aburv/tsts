01f690192d6fe25b9473470f3d6e8ff70b013315
Gurunathan Ramalingam <raghuramalingam96@gmail.com>
2023-12-16 11:58:13 +0530

    [Angular] Add [UI Component] Button

--- app/web-interface/src/app/app.module.ts
+++ app/web-interface/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonComponent } from './components/button/button.component';
 import { IconComponent } from './components/icon/icon.component';
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -12,6 +13,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ButtonComponent,
     IconComponent,
     DashboardComponent
   ],
