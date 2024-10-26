dbbbbc4db67cfe4c6f4319aebeeae98ff655123c
Gurunathan Ramalingam <gurunathan.r@aburv.com>
2024-10-26 16:01:22 +0530

    [Server] Add Image API to add new and retrieve by id and size

--- app/server/src/app.py
+++ app/server/src/app.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 
 from src.app_check import PING_BLUEPRINT
 from src.device.controller import DEVICE_BLUEPRINT
+from src.image.controller import IMAGE_BLUEPRINT
 from src.user.controller import USER_BLUEPRINT
 
 APP = Flask(__name__)
@@ -21,3 +22,5 @@ APP.register_blueprint(PING_BLUEPRINT, url_prefix="/api/ping")
 
 APP.register_blueprint(USER_BLUEPRINT, url_prefix="/api/user")
 APP.register_blueprint(DEVICE_BLUEPRINT, url_prefix="/api/device")
+
+APP.register_blueprint(IMAGE_BLUEPRINT, url_prefix="/api/image")
