91b8dd6cfd195de2f9675cb5cfe6bca9dd6392ab
Gurunathan Ramalingam <gurunathan.r@aburv.com>
2025-01-18 16:51:58 +0530

    [Script] Fix  Coverage script to omit in coverage

--- scripts/coverage_python.sh
+++ scripts/coverage_python.sh
@@ -30,7 +30,7 @@ lint_score=$(echo "$last_second_value" | awk -F'/' '{print $1}')
 
 echo "Pylint score: $lint_score"
 
-if (( $(echo "$lint_score > $required_score" | bc -l) ))
+if (( $(echo "$lint_score >= $required_score" | bc -l) ))
 then
     echo "Pylint score is excellent: $lint_score"
 else
@@ -43,7 +43,7 @@ python -m grpc_tools.protoc -I./proto --python_out=. --grpc_python_out=. ./proto
 
 echo "Start Testing"
 
-coverage run --source=. --omit=test/\* -m unittest discover -s test 
+coverage run --source=. -m unittest discover -s test 
 coverage json
 json_val=`jq '.totals.percent_covered' coverage.json` 
 coverage_percentage=$(echo "$json_val" | sed 's/\..*//')
