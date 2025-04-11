0a551c8596fdc566e7c7651bb938b3cbee51becc
Gurunathan Ramalingam <gurunathan.r@aburv.com>
2025-04-11 22:30:03 +0530

    [Angular] Refactor lint and unwanted statements and imports

--- app/web-interface/src/app/pages/dashboard/dashboard.component.spec.ts
+++ app/web-interface/src/app/pages/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,3 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
@@ -10,7 +9,6 @@ describe('DashboardComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [DashboardComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     });
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
