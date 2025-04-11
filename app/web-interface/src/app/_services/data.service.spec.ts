463f664de95398d751b16d604e3b15f086de8b59
Gurunathan Ramalingam <gurunathan.r@aburv.com>
2025-04-11 23:05:56 +0530

    [Angular] Add On get call, getting 401 status, token refreshed and retry once on the actual call

--- app/web-interface/src/app/_services/data.service.spec.ts
+++ app/web-interface/src/app/_services/data.service.spec.ts
@@ -14,7 +14,9 @@ describe('DataService', () => {
 
         const pingSpy = jasmine.createSpyObj('PingService', ['ping']);
 
-        const service = new DataService(httpSpy, pingSpy);
+        const userDataSpy = jasmine.createSpyObj('UserDataService', ['refreshUserToken']);
+
+        const service = new DataService(httpSpy, pingSpy, userDataSpy);
         
         const actual = service.get('url/path');
 
@@ -39,7 +41,9 @@ describe('DataService', () => {
 
         const pingSpy = jasmine.createSpyObj('PingService', ['ping']);
 
-        const service = new DataService(httpSpy, pingSpy);
+        const userDataSpy = jasmine.createSpyObj('UserDataService', ['refreshUserToken']);
+
+        const service = new DataService(httpSpy, pingSpy, userDataSpy);
 
         const actual = service.get('url/path');
 
@@ -66,7 +70,9 @@ describe('DataService', () => {
 
         const pingSpy = jasmine.createSpyObj('PingService', ['ping']);
 
-        const service = new DataService(httpSpy, pingSpy);
+        const userDataSpy = jasmine.createSpyObj('UserDataService', ['refreshUserToken']);
+
+        const service = new DataService(httpSpy, pingSpy, userDataSpy);
 
         const actual = service.get('url/path');
 
@@ -84,16 +90,82 @@ describe('DataService', () => {
         expect(pingSpy.ping).toHaveBeenCalledOnceWith();
     });
 
+    it('Should call refresh token and retry on success get when 401 status on get call', () => {
+        spyOn(Config, 'getDomain').and.returnValue('https://localhost/api/');
+        spyOn(Config, 'getHeaders').and.returnValue({ headers: { header: 'header' } });
+
+        const responseData = { 'data': [] };
+        const httpSpy = jasmine.createSpyObj('HttpClient', ['get']);
+        httpSpy.get.and.returnValues(
+            throwError(() => { return { status: 401, statusText: "UnAuthenticated" } }),
+            of(responseData)
+        );
+
+        const pingSpy = jasmine.createSpyObj('PingService', ['ping']);
+
+        const userDataSpy = jasmine.createSpyObj('UserDataService', ['refreshUserToken']);
+        userDataSpy.refreshUserToken.and.returnValue(of(true))
+
+        const service = new DataService(httpSpy, pingSpy, userDataSpy);
+
+        const actual = service.get('url/path');
+
+        expect(httpSpy.get).toHaveBeenCalledOnceWith(
+            'https://localhost/api/url/path',
+            {
+                headers: { header: 'header' }
+            }
+        );
+
+        actual.subscribe(res => {
+            expect(res).toBe(responseData);
+        })
+
+        expect(userDataSpy.refreshUserToken).toHaveBeenCalledOnceWith();
+    });
+
+    it('Should call refresh token and no retry on failure get when 401 status on get call', () => {
+        spyOn(Config, 'getDomain').and.returnValue('https://localhost/api/');
+        spyOn(Config, 'getHeaders').and.returnValue({ headers: { header: 'header' } });
+
+        const httpSpy = jasmine.createSpyObj('HttpClient', ['get']);
+        httpSpy.get.and.returnValue(throwError(() => { return { status: 401, statusText: "UnAuthenticated" } }));
+
+        const pingSpy = jasmine.createSpyObj('PingService', ['ping']);
+
+        const userDataSpy = jasmine.createSpyObj('UserDataService', ['refreshUserToken']);
+        userDataSpy.refreshUserToken.and.returnValue(of(false))
+
+        const service = new DataService(httpSpy, pingSpy, userDataSpy);
+
+        const actual = service.get('url/path');
+
+        expect(httpSpy.get).toHaveBeenCalledOnceWith(
+            'https://localhost/api/url/path',
+            {
+                headers: { header: 'header' }
+            }
+        );
+
+        actual.subscribe(res => {
+            expect(res).toBe('');
+        })
+
+        expect(userDataSpy.refreshUserToken).toHaveBeenCalledOnceWith();
+    });
+
     it('Should call ping the server on other statuses on get call', () => {
         spyOn(Config, 'getDomain').and.returnValue('https://localhost/api/');
         spyOn(Config, 'getHeaders').and.returnValue({ headers: { header: 'header' } });
 
         const httpSpy = jasmine.createSpyObj('HttpClient', ['get']);
-        httpSpy.get.and.returnValue(throwError(() => { return { status: 401, statusText: "Not Found" } }));
+        httpSpy.get.and.returnValue(throwError(() => { return { status: 305, statusText: "Not Found" } }));
 
         const pingSpy = jasmine.createSpyObj('PingService', ['ping']);
 
-        const service = new DataService(httpSpy, pingSpy);
+        const userDataSpy = jasmine.createSpyObj('UserDataService', ['refreshUserToken']);
+
+        const service = new DataService(httpSpy, pingSpy, userDataSpy);
 
         const actual = service.get('url/path');
 
@@ -109,6 +181,7 @@ describe('DataService', () => {
         })
 
         expect(pingSpy.ping).not.toHaveBeenCalledOnceWith();
+        expect(userDataSpy.refreshUserToken).not.toHaveBeenCalledOnceWith();
     });
 
     it('Should send data and return response data if not isServerDown on post call', () => {
@@ -122,7 +195,10 @@ describe('DataService', () => {
         const pingSpy = jasmine.createSpyObj('PingService', ['ping']);
         PingService.isServerDown.set(false);
 
-        const service = new DataService(httpSpy, pingSpy);
+        const userDataSpy = jasmine.createSpyObj('UserDataService', ['refreshUserToken']);
+        userDataSpy.refreshUserToken.and.returnValue(of(''))
+
+        const service = new DataService(httpSpy, pingSpy, userDataSpy);
 
         const actual = service.post('url/path', {});
 
@@ -150,7 +226,10 @@ describe('DataService', () => {
         const pingSpy = jasmine.createSpyObj('PingService', ['ping']);
         PingService.isServerDown.set(true);
 
-        const service = new DataService(httpSpy, pingSpy);
+        const userDataSpy = jasmine.createSpyObj('UserDataService', ['refreshUserToken']);
+        userDataSpy.refreshUserToken.and.returnValue(of(''))
+
+        const service = new DataService(httpSpy, pingSpy, userDataSpy);
 
         const actual = service.post('url/path', {});
 
--- app/web-interface/src/app/_services/data.service.ts
+++ app/web-interface/src/app/_services/data.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Config } from '../config';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, of, switchMap } from 'rxjs';
 import { PingService } from './ping.service';
+import { UserDataService } from './UserData.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,18 +11,31 @@ import { PingService } from './ping.service';
 export class DataService {
   constructor(
     private http: HttpClient,
-    private pingService: PingService
+    private pingService: PingService,
+    private userData: UserDataService
   ) { }
 
-  get(url: string): Observable<any> {
-    return this.http.get(Config.getDomain() + url, Config.getHeaders()).pipe(
+  get(path: string): Observable<any> {
+    const url = Config.getDomain() + path;
+    return this.http.get(url, Config.getHeaders()).pipe(
       catchError((error: HttpErrorResponse) => {
         if (!(error.error instanceof ErrorEvent)) {
+          if (error.status === 401) {
+            return this.userData.refreshUserToken().pipe(
+              switchMap((isDone: boolean) => {
+                if (isDone) {
+                  return this.http.get(url, Config.getHeaders());
+                }
+                return of('');
+              })
+            );
+          }
           if (error.status === 404 || error.status === 0) {
             this.pingService.ping();
+            return of('');
           }
         }
-        return of('')
+        return of('');
       })
     );
   }
